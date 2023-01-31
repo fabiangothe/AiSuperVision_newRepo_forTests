483169d20682593bfa50c0f62a7a96ee3e0cf730
fabiangothe <fabiangothe@gmail.com>
2023-01-31 14:33:24 +0100

    changes

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import sys
 
 
 
-extensions = ['sphinx_sqlalchemy', 'sphinxcontrib.sadisp', 'sphinx.ext.inheritance_diagram', 'sphinxcontrib-sqlalchemyviz', 'sqlaviz']
+extensions = ['sphinx_sqlalchemy', 'sphinxcontrib.sadisp', 'sphinx.ext.inheritance_diagram', 'sqlaviz']
 
 html_theme = "furo"
 
